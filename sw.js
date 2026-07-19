e4204b1a1bd2e2c4899607f782f9814c9c2e93d7
Y'allternative Living <y.allternative.living@gmail.com>
2026-07-19 13:55:13 -0700

    Style: increase container spacing, shorten homepage eyebrow to remove pill redundancy, and bump cache to v12

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  */
 
 /** @const {string} Cache name key, updated on assets release. */
-const CACHE_NAME = "yallternative-cache-v11";
+const CACHE_NAME = "yallternative-cache-v12";
 
 /** @const {!Array<string>} Array of absolute URLs to be cached on installation. */
 const ASSETS_TO_CACHE = [
